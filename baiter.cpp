2d0c2dc047792e23c7684d45352c9b0102682839
mrborges23 <ruiborges23@gmail.com>
2020-04-23 17:54:08 +0200

    making the code more efficient

--- baiter.cpp
+++ baiter.cpp
@@ -4,7 +4,6 @@
 #include <sstream>
 #include <cmath>
 #include <armadillo>
-#include <chrono>
 
 #include <boost/math/special_functions/gamma.hpp>
 #include <boost/math/special_functions/binomial.hpp>
@@ -12,19 +11,36 @@
 using namespace std;
 using namespace arma;
 
-//functions declaration
-vec trajectory_probability(mat prob_matrix, vec vector1, vec vector2);
-vec moran_states_distribution(double N, int allele_coverage, int total_coverage) ;
-mat counts_to_moran_states(Col<int> allele_counts, Col<int> total_counts,int number_time_points, int number_replicates, double N, int order);
 
-mat pomo_rate_matrix(double N, double sigma);
-double log_likelihood_trajectory(double N, mat trajectory_matrix, vec indexes, mat prob_matrices);
-double log_likelihood_trajectories(double N, double sigma, int number_replicates,int number_time_points, vec time, mat trajectories_matrix);
-double log_posterior(double N, double sigma, mat trajectories_matrix,int number_replicates, int number_time_points, vec time, vec prior_parameters);
-vec sigma_posterior2(double N, vec time, int number_time_points, int number_replicates,mat trajectories_matrix, vec prior_parameters);
+// global variables
+
+char nuc_bases[] = "ATCGND";
+
+int n_replicates;
+int n_time_points;
+int n_loci;
+int header;  
+int order;
+
+double N;
+
+vec time_vector;
+vec prior_parameters;
+
+//functions declaration
+void   read_sync_file_line          (ifstream &inFile, string &info, Col<int> &allele_counts, Col<int> &total_counts ) ;
+vec    trajectory_probability       (mat &prob_matrix, vec &vector1, vec &vector2);
+void   moran_states_distribution    (vec &moran_distribution, double &allele_coverage, double &total_coverage) ;
+void   counts_to_moran_states       (mat &allele_trajectories, Col<int> &allele_counts, Col<int> &total_counts);
+
+mat    pomo_rate_matrix             (double &sigma);
+double log_likelihood_trajectory    (mat &trajectory_matrix, vec &indexes, mat &prob_matrices);
+double log_likelihood_trajectories  (double &sigma, mat &trajectories_matrix);
+double log_posterior                (double &sigma, mat &trajectories_matrix);
+void   sigma_posterior2             (mat &trajectories_matrix, string &info);
 
 // functions for gamma and binomial distributions
-double dbinom(double k,double N, double p);
+double dbinom(double k,double C, double p);
 
 
 
@@ -51,16 +67,8 @@ int main (int argc, char *argv[]){
   string sync_file;
   string output_file;
 
-  int n_replicates;
-  int n_loci;
-
-  int header;  
-  int order;
-
   string time_points;
   string prior_vector;
-
-  double N;
 
   control >> name >> sync_file;
   control >> name >> header;  
@@ -75,35 +83,19 @@ int main (int argc, char *argv[]){
   control.close();
 
   // vector time and n_time_points
-  vec time = vec(time_points);
-  int n_time_points = time.size();
+  time_vector = vec(time_points);
+  n_time_points = time_vector.size();
 
   // vector of prior parameters
-  vec prior_parameters = vec(prior_vector);
+  prior_parameters = vec(prior_vector);
 
-  
+  // some info is printed in the terminal
   cout << "  Information received: \n   - sync file:                 " << sync_file <<"\n   - number loci:               " << n_loci << "\n   - number replicates:         " << n_replicates << "\n   - time points:               " << time_points << "\n   - number time points:        " << n_time_points << "\n   - effective population size: " << N << "\n   - prior parameters:          " << prior_vector << "\n\n";
 
   // output file
   ofstream outFile;
   outFile.open (output_file);
   outFile << "chromosome\tposition\treference\tsigma\tlogBF\talpha\tbeta\n";
-
-  //useful variables
-  int cols   = n_replicates*n_time_points+3;
-  int ref_index; 
-
-  string C[cols];
-  string H[cols];
-
-  Col<int> counts(4);
-  uvec indices;
-
-  Col<int> allele_counts = zeros<Col<int>>(n_replicates*n_time_points);
-  Col<int> total_counts  = zeros<Col<int>>(n_replicates*n_time_points);
-
-  //bases
-  char nuc_bases[] = "ATCGND";
 
   //opens sync file
   //checks whether sync file is open
@@ -115,62 +107,40 @@ int main (int argc, char *argv[]){
   }
 
   //skip the first row if there is an header
+  string H[n_replicates*n_time_points+3];
   if (header == 1){
-    for(int j = 0; j < cols; j++){
+    for(int j = 0; j < (n_replicates*n_time_points+3); j++){
       inFile >> H[j];
     }
   }
 
   cout << "  Bait-ER has started!\n\n";
 
+  // some useful variables
+  string info1; 
+  string info2; 
+
+  Col<int> allele_counts(n_replicates*n_time_points);
+  Col<int> total_counts(n_replicates*n_time_points);
+  mat allele_trajectories((N+1)*n_replicates,n_time_points);
   //reads sync file line by line
   for(int i = 0; i < n_loci; i++){
-    for(int j = 0; j < cols; j++){
-      inFile >> C[j];
-        
-        //first replicate and time point are used to get the highest two counts
-        //these correspont to the alleles that will be tracked for the remaining 
-        //time points and replicates
-        if (j == 3){
+    
+    // reset the objects
+    allele_counts.zeros();
+    total_counts.zeros();
+    allele_trajectories.zeros();
 
+    // update info1 and counts
+    read_sync_file_line(inFile, info1, allele_counts, total_counts ) ;
+    
+    // update trajectories matrix
+    counts_to_moran_states(allele_trajectories,allele_counts,total_counts);
+    
+    // posterior estimates and statistics
+    sigma_posterior2(allele_trajectories,info2);
 
-            istringstream ss(C[j]);
-            string token;
-            
-            Col<int> counts(4);
-
-            for (int k = 0; k < 4; k++){
-              getline(ss, token, ':');
-              counts(k) = stoi(token);
-            }
-
-            //the highest two counts
-            indices = sort_index(counts,"descend");
-
-            allele_counts(0) = counts(indices(0));
-            total_counts(0)  = counts(indices(0)) + counts(indices(1));
-
-        } else if (j>3){
-
-        	istringstream ss(C[j]);
-            string token;
-
-            Col<int> counts(4);
-
-            for (int k = 0; k < 4; k++){
-              getline(ss, token, ':');
-              counts(k) = stoi(token);
-            }
-
-            allele_counts(j-3) = counts(indices(0));
-            total_counts(j-3)  = counts(indices(0)) + counts(indices(1));
-        }
-      }
-
-    mat trajectories_matrix = counts_to_moran_states(allele_counts,total_counts,n_time_points,n_replicates,N,order);
-    vec output = sigma_posterior2(N,time,n_time_points,n_replicates,trajectories_matrix,prior_parameters);
-
-    outFile << C[0] << "\t" << C[1] << "\t" << nuc_bases[indices(0)] << nuc_bases[indices(1)] << "\t" << output(0) << "\t" << output(1) << "\t" << output(2) << "\t" << output(3) << "\n"; 
+    outFile << info1 << info2 << "\n"; 
   }
 
   // close in and output files
@@ -195,7 +165,7 @@ N  population size
 sigma selection coefficient
 */
 
-mat pomo_rate_matrix(double N, double sigma) {
+mat pomo_rate_matrix( double &sigma) {
   
   // useful quantities
   int number_pomo_states = N+1;
@@ -231,7 +201,7 @@ prob_matrix: the transpose of the pomo rate matrix, in the increment of time bet
  
  */
 
-vec trajectory_probability(mat prob_matrix, vec vector1, vec vector2) {
+vec trajectory_probability(mat &prob_matrix, vec &vector1, vec &vector2) {
 
   // calculates the conditional probabilities of observing the pomo state vector vector2 in time t2 
   // given that the pomo state vector in t1 was vector1
@@ -257,13 +227,12 @@ vec trajectory_probability(mat prob_matrix, vec vector1, vec vector2) {
  times:
  */
 
-double log_likelihood_trajectory(double N, mat trajectory_matrix, vec indexes, mat prob_matrices) {
+double log_likelihood_trajectory(  mat &trajectory_matrix, vec &indexes, mat &prob_matrices) {
   
   // useful quantities
   vec vector1 =trajectory_matrix.col(0);
-  int number_time_points = trajectory_matrix.n_cols;
   
-  for (int i=0; i<(number_time_points-1); i++){
+  for (int i=0; i<(n_time_points-1); i++){
     
     // nth observed vector
     vec vector2 = trajectory_matrix.col(i+1);
@@ -290,20 +259,20 @@ double log_likelihood_trajectory(double N, mat trajectory_matrix, vec indexes, m
  the replicates are row binded
 */
 
-double log_likelihood_trajectories(double N, double sigma, int number_replicates,int number_time_points, vec time, mat trajectories_matrix) {
+double log_likelihood_trajectories( double &sigma,  mat &trajectories_matrix) {
 
   //avoids calculating the exp matrix unecessary number of times as it 
   //checks whether the increments between any two time points is equal
-  vec indexes(number_time_points-1);
+  vec indexes(n_time_points-1);
   indexes.zeros();
 
-  mat rate_matrix = pomo_rate_matrix(N,sigma);
+  mat rate_matrix = pomo_rate_matrix(sigma);
 
   // calculates increments
-  vec increments(number_time_points-1);
+  vec increments(n_time_points-1);
   
-  for (int i=0; i<(number_time_points-1); i++){
-    increments(i) = time(i+1) - time(i);
+  for (int i=0; i<(n_time_points-1); i++){
+    increments(i) = time_vector(i+1) - time_vector(i);
   }
   
   // evaluates prob_matrix for all the unique increments
@@ -318,16 +287,16 @@ double log_likelihood_trajectories(double N, double sigma, int number_replicates
       indexes.elem(find(increments == delta)) += i;
   }
 
-  vec log_likelihood_replicates(number_replicates);
+  vec log_likelihood_replicates(n_replicates);
   log_likelihood_replicates.zeros();
   
-  for (int i=0; i < number_replicates; i++){
+  for (int i=0; i < n_replicates; i++){
     
     // preparing trajectories_matrix, for replicate number i
     mat trajectory_matrix = trajectories_matrix.rows(i*N+i,(i+1)*N+i);
 
     // calculates the likelihood of each trajectory
-    log_likelihood_replicates(i) = log_likelihood_trajectory(N, trajectory_matrix, indexes, prob_matrix_keeper);
+    log_likelihood_replicates(i) = log_likelihood_trajectory( trajectory_matrix, indexes, prob_matrix_keeper);
   }
 
   // total likelihood
@@ -345,11 +314,12 @@ double log_likelihood_trajectories(double N, double sigma, int number_replicates
  
 */
 
-double log_posterior(double N, double sigma, mat trajectories_matrix,int number_replicates, int number_time_points, vec time, vec prior_parameters) {
+double log_posterior(  double &sigma, mat &trajectories_matrix ) {
 
    // adds the log likelihood to the log prior
-   double log_total = prior_parameters(0)*log(prior_parameters(1))+(prior_parameters(0)-1.0)*log(1.0+sigma)-prior_parameters(1)*(1.0+sigma)-lgamma(prior_parameters(0)) +
-                      log_likelihood_trajectories(N, sigma, number_replicates , number_time_points , time , trajectories_matrix);
+   double log_total = log_likelihood_trajectories(sigma , trajectories_matrix)
+                      + prior_parameters(0)*log(prior_parameters(1))+(prior_parameters(0)-1.0)*log(1.0+sigma)-prior_parameters(1)*(1.0+sigma)-lgamma(prior_parameters(0));
+                      
 
   return log_total;
 }
@@ -360,22 +330,22 @@ Calculates a bayes factor against sigma=0
 Constitutes a test of neutrality
 */
 
-vec sigma_posterior2(double N, vec time, int number_time_points, int number_replicates,mat trajectories_matrix, vec prior_parameters) {
+void sigma_posterior2( mat &trajectories_matrix, string &info) {
   
   // calculates increments
   mat increments; 
-  increments.zeros(number_replicates,number_time_points-1);
+  increments.zeros(n_replicates,n_time_points-1);
   vec pomo_states = regspace(0,1,N);
   
   double n1,n2,t1,t2;
   
-  for (int i=0;i<number_replicates; i++){
-    for (int j=1;j<number_time_points;j++){
+  for (int i=0;i<n_replicates; i++){
+    for (int j=1;j<n_time_points;j++){
       
       n1 = sum(pomo_states%trajectories_matrix(span(i*N+i,(i+1)*N+i),j-1));
       n2 = sum(pomo_states%trajectories_matrix(span(i*N+i,(i+1)*N+i),j));
-      t1 = time(j-1);
-      t2 = time(j);
+      t1 = time_vector(j-1);
+      t2 = time_vector(j);
       
       increments(i,j-1) = (n2/n1-1)/(t2-t1);
       
@@ -386,12 +356,15 @@ vec sigma_posterior2(double N, vec time, int number_time_points, int number_repl
   double m_sigma  = mean(mean(increments));
   double sd_sigma = mean(mean(abs(increments-m_sigma)));
   
-  // sometimes de emprirical variance gets too small and numerical problems 
+  //cout << m_sigma << " " << sd_sigma << " "; 
+
+  // sometime_vectors de emprirical variance gets too small and numerical problems 
   // on fiting alpha and beta arise (e.g. alpha is negative or beta is null) 
+  /*
   if (sd_sigma < 0.001) {
     sd_sigma = 0.001;
   }
-
+  */
 
   // fit the gamma distribution
   vec grid;
@@ -401,8 +374,17 @@ vec sigma_posterior2(double N, vec time, int number_time_points, int number_repl
 
   vec log_likelihood(grid.n_elem);
   for (int i =0; i < grid.n_elem; i++){
-    log_likelihood(i) = log_posterior(N,grid(i),trajectories_matrix,number_replicates,number_time_points,time,prior_parameters);
+    log_likelihood(i) = log_posterior(grid(i),trajectories_matrix);
   }
+  
+  /*
+  for (int i =0; i < grid.n_elem; i++){
+    cout << grid(i) << " "; 
+  }
+  for (int i =0; i < grid.n_elem; i++){
+    cout << log_likelihood(i) << " "; 
+  }
+  */
 
   vec xi = grid + 1.0;
   vec yi = log_likelihood;
@@ -422,6 +404,7 @@ vec sigma_posterior2(double N, vec time, int number_time_points, int number_repl
   double beta   = -(si3*si4*si4-si2*si5*si4-si1*si6*si4+si5*si6+si1*si2*si7-si3*si7)/d;
 
   // another step to avoid numerical problems
+  /*
   if (alpha < 0) {
 
     sd_sigma = 0.01;
@@ -430,7 +413,7 @@ vec sigma_posterior2(double N, vec time, int number_time_points, int number_repl
 
     // calculate the log_likelihood
     for (int i =0; i < grid.n_elem; i++){
-      log_likelihood(i) = log_posterior(N,grid(i),trajectories_matrix,number_replicates,number_time_points,time,prior_parameters);
+      log_likelihood(i) = log_posterior(grid(i),trajectories_matrix);
     }
 
     xi = grid + 1.0;
@@ -451,20 +434,16 @@ vec sigma_posterior2(double N, vec time, int number_time_points, int number_repl
     beta   = -(si3*si4*si4-si2*si5*si4-si1*si6*si4+si5*si6+si1*si2*si7-si3*si7)/d;
 
   }
+  */
 
   // posterior statistics
-  double mean              = alpha/beta-1;
+  double average           = alpha/beta-1;
   double log_bayes_factor  = log(boost::math::gamma_q(alpha,beta)) - log(boost::math::gamma_p(alpha,beta)); 
   //+                          log(boost::math::gamma_p(prior_parameters(0),prior_parameters(1))) - log(boost::math::gamma_q(prior_parameters(0),prior_parameters(1)));
-  
-  // exports posterior statistics as a vector
-  vec output(4);
-  output(0) = mean;
-  output(1) = log_bayes_factor;
-  output(2) = alpha;
-  output(3) = beta;
 
-  return output;
+  //cout << alpha << " " << beta << " " << average << " " << log_bayes_factor << " " << endl;
+  // exports posterior as a string
+  info = "\t" + to_string(average) + "\t" + to_string(log_bayes_factor) + "\t" + to_string(alpha) + "\t" + to_string(beta); 
 
 }
 
@@ -480,48 +459,43 @@ vec sigma_posterior2(double N, vec time, int number_time_points, int number_repl
 */
 
 
-mat counts_to_moran_states(Col<int> allele_counts, Col<int> total_counts,int number_time_points, int number_replicates, double N,int order) {
+void counts_to_moran_states(mat &allele_trajectories, Col<int> &allele_counts, Col<int> &total_counts) {
 
   // usefull quantitie
-  mat allele_trajectories((N+1)*number_replicates,number_time_points);
-  allele_trajectories.zeros();
-    
   vec moran_distribution(N+1); 
-  moran_distribution.zeros();
-    
+  
   // calculates the probability of each moran state given the observed counts  
   if (order == 0) {
   int index = 0; 
-    for (int t=0; t<number_time_points; t++){
-      for (int r=0; r<number_replicates; r++){
+    for (int t=0; t<n_time_points; t++){
+      for (int r=0; r<n_replicates; r++){
       
         double allele_coverage = allele_counts(index);
         double total_coverage  = total_counts(index);
-      
-        moran_distribution = moran_states_distribution(N, allele_coverage, total_coverage);
+
+        moran_states_distribution(moran_distribution, allele_coverage, total_coverage);
         allele_trajectories(span(r*N+r,(r+1)*N+r),t) = moran_distribution;
         index = index+1;
+
       }
     }
   }
 
   if (order == 1) {
   int index = 0; 
-    for (int r=0; r<number_replicates; r++){
-      for (int t=0; t<number_time_points; t++){
+    for (int r=0; r<n_replicates; r++){
+      for (int t=0; t<n_time_points; t++){
       
         double allele_coverage = allele_counts(index);
         double total_coverage  = total_counts(index);
       
-        moran_distribution = moran_states_distribution(N, allele_coverage, total_coverage);
+        moran_states_distribution(moran_distribution,allele_coverage, total_coverage);
         allele_trajectories(span(r*N+r,(r+1)*N+r),t) = moran_distribution;
         index = index+1;
+
       }
     }
   }
-
-  
-  return allele_trajectories;
   
 }
 
@@ -540,20 +514,76 @@ mat counts_to_moran_states(Col<int> allele_counts, Col<int> total_counts,int num
  moran_states_distribution(N,allele_coverage,total_coverage)
  */
 
-vec moran_states_distribution(double N, int allele_coverage, int total_coverage) {
-  
-  //usefull quantities
-  int number_pomo_states =  N+1;
-  vec moran_distribution(number_pomo_states);
+void moran_states_distribution(vec &moran_distribution,  double &allele_coverage, double &total_coverage) {
   
   // populates the moran distribution using the inverse binomial sampling
-  for (int i=0; i<number_pomo_states;i++){
+  for (int i=0; i<(N+1) ;i++){
     moran_distribution(i) = dbinom(allele_coverage,total_coverage,i/N);
   }
   
   moran_distribution = moran_distribution/sum(moran_distribution);
-  return moran_distribution;
+
 }
+
+/*
+  Reads a line of the sync file
+*/
+
+void read_sync_file_line(ifstream &inFile, string &info, Col<int> &allele_counts, Col<int> &total_counts ) {
+
+  string chromosome;
+  string position;
+  string counts_str;
+
+  inFile >> chromosome;
+  inFile >> position;
+  inFile >> counts_str;
+
+  // goes through all the cols and saves content to C[j]
+  Mat<int> counts_matrix(n_replicates*n_time_points,4);
+
+  for(int j = 0; j < (n_replicates*n_time_points); j++){
+
+    // counts_str = "c1:c2:c3:C4:c5:c6"
+    // count_str  = "c1"
+    inFile >> counts_str;
+    stringstream reader(counts_str);
+    string count_str;
+
+    for (int k = 0; k < 4; k++){
+      getline(reader, count_str, ':');
+      counts_matrix(j,k) = stoi(count_str);
+    }
+
+  }
+
+  Row<int> sum_row = sum(counts_matrix,0);
+
+  //uvec test_biallelic = find(sum_row > 0);
+
+  // tests if the site
+  // 0: has no counts for any of the nucleotide bases
+  // 1: is monomorphic
+  // 2: is biallelic
+  // 3: is triallelic
+
+  uvec sort_sum_row = sort_index(sum_row);
+    
+  // Finds the two alleles that have the 
+  char a1,a2;
+  a1 = sort_sum_row(3);
+  a2 = sort_sum_row(2);
+
+  //cout << chromosome << " " << position << " " << nuc_bases[a1] << nuc_bases[a2] << " ";
+
+  info = chromosome + "\t" + position + "\t" + nuc_bases[a1] + ":" + nuc_bases[a2];   
+  // fills the total coverage and the allele coverage matrices
+  allele_counts = counts_matrix.col(a1);
+  total_counts  = sum(counts_matrix,1);
+
+}
+
+
 
 
 
@@ -561,8 +591,8 @@ vec moran_states_distribution(double N, int allele_coverage, int total_coverage)
  Some usefull distributions
 */
 
-double dbinom(double k,double N, double p){
-  double probability = boost::math::binomial_coefficient<double>(N,k)*pow(p,k)*pow(1-p,N-k);
+double dbinom(double k, double C,  double p){
+  double probability = boost::math::binomial_coefficient<double>(C,k)*pow(p,k)*pow(1-p,C-k);
   return probability;
 }
 
